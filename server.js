5292b0cde40c196e94134516c39a84407b79b127
Joseph Allen <sephallen@gmail.com>
2015-02-23 14:00:11 +0000

    Door lock functionality complete

--- server.js
+++ server.js
@@ -18,13 +18,25 @@ board.on("ready", function() {
     res.send('Light on');
     status.light = 'ON';
     led.on();
-    io.emit('ack button status', { statusLight: status.light });
+    io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
   });
   app.get('/lightoff', function(req, res) {
     res.send('Light off');
     status.light = 'OFF';
     led.off();
-    io.emit('ack button status', { statusLight: status.light });
+    io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
+  });
+  app.get('/lock', function(req, res) {
+    res.send('Door locked');
+    status.door = 'locked';
+    servo.to(1);
+    io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
+  });
+  app.get('/unlock', function(req, res) {
+    res.send('Door unlocked');
+    status.door = 'unlocked';
+    servo.max();
+    io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
   });
   app.get('/json', function(req, res) {
     res.json(status);
@@ -39,16 +51,16 @@ board.on("ready", function() {
       socket.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
 
       socket.on('button update event', function (data) {
-          console.log(data.statusLight);
+          // console.log(data.statusLight);
 
           //acknowledge with inverted status,
           //to toggle button text in client
           if(data.statusLight == 'OFF') {
-              console.log("OFF->ON");
+              // console.log("OFF->ON");
               status.light = 'ON';
               led.on();
           } else if(data.statusLight == 'ON') {
-              console.log("ON->OFF");
+              // console.log("ON->OFF");
               status.light = 'OFF';
               led.off();
           }
