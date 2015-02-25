6c1b57a3777145cee2afdeee7066566883e6863c
Joseph Allen <sephallen@gmail.com>
2015-02-25 20:19:34 +0000

    Added thermostat control and LCD display

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 var socket = io.connect(document.location.href);
-var storedTemp =
+var oldThermTemp = 0;
 
 socket.on('on connection', function (data) {
 });
@@ -13,41 +13,49 @@ socket.on('your id',function(data) {
 socket.on('ack button status', function (data) {
     // console.log("statusLight: " + data.statusLight);
     // console.log("statusDoor: " + data.statusDoor);
-    // console.log("statusTemp: " + data.statusTemp);
+    // console.log(oldThermTemp);
+    if (oldThermTemp != data.statusThermostat) {
+      document.getElementById("thermSlider").value = data.statusThermostat;
+      oldThermTemp = data.statusThermostat;
+    }
+    if (oldThermTemp != document.getElementById("thermSlider").value) {
+      socket.emit('button update event', { statusThermostat: document.getElementById("thermSlider").value });
+    }
+
     document.getElementById("tempDisplay").firstChild.data="The current house temperature is " + data.statusTemp +"°C";
 
     if(data.statusLight == 'OFF') {
-        document.getElementById("lightSwitch").firstChild.data="Turn on the light";
-        document.getElementById("lightSwitch").className="waves-effect waves-light btn green white-text";
+        document.getElementById("lightSwitch").firstChild.data = "Turn on the light";
+        document.getElementById("lightSwitch").className = "waves-effect waves-light btn green white-text";
     }else if(data.statusLight == 'ON') {
-        document.getElementById("lightSwitch").firstChild.data="Turn off the light";
-        document.getElementById("lightSwitch").className="waves-effect waves-light btn red white-text";
+        document.getElementById("lightSwitch").firstChild.data = "Turn off the light";
+        document.getElementById("lightSwitch").className = "waves-effect waves-light btn red white-text";
     }
     if(data.statusDoor == 'locked'){
-        document.getElementById("doorLock").firstChild.data="Unlock the door";
-        document.getElementById("doorLock").className="waves-effect waves-light btn green white-text";
+        document.getElementById("doorLock").firstChild.data = "Unlock the door";
+        document.getElementById("doorLock").className = "waves-effect waves-light btn green white-text";
     }else if (data.statusDoor == 'unlocked') {
-        document.getElementById("doorLock").firstChild.data="Lock the door";
-        document.getElementById("doorLock").className="waves-effect waves-light btn red white-text";
+        document.getElementById("doorLock").firstChild.data = "Lock the door";
+        document.getElementById("doorLock").className = "waves-effect waves-light btn red white-text";
     }
 });
 
 function toggleLight(button)
 {
- if(document.getElementById("lightSwitch").firstChild.data=="Turn off the light"){
+ if(document.getElementById("lightSwitch").firstChild.data == "Turn off the light"){
   socket.emit('button update event', { statusLight: 'ON' });
  }
- else if(document.getElementById("lightSwitch").firstChild.data=="Turn on the light"){
+ else if(document.getElementById("lightSwitch").firstChild.data == "Turn on the light"){
   socket.emit('button update event', { statusLight: 'OFF' });
  }
 }
 
 function toggleLock(button)
 {
- if(document.getElementById("doorLock").firstChild.data=="Lock the door"){
+ if(document.getElementById("doorLock").firstChild.data == "Lock the door"){
   socket.emit('button update event', { statusDoor: 'unlocked' });
  }
- else if(document.getElementById("doorLock").firstChild.data=="Unlock the door"){
+ else if(document.getElementById("doorLock").firstChild.data == "Unlock the door"){
   socket.emit('button update event', { statusDoor: 'locked' });
  }
 }
--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var arduino = require('johnny-five')
 
 board.on("ready", function() {
 
-  var status = {light:"OFF", door:"locked", temp:"0"};
+  var status = {light:"OFF", door:"locked", temp:"0", thermostat:"0"};
+
   function statusEmit() {
-    io.emit('ack button status', { statusLight: status.light, statusDoor: status.door, statusTemp: status.temp });
+    io.emit('ack button status', { statusLight: status.light, statusDoor: status.door, statusTemp: status.temp,
+    statusThermostat: status.thermostat });
   }
 
   var led = new arduino.Led(13);
@@ -22,6 +24,20 @@ board.on("ready", function() {
   temperature.on("data", function(err, data) {
     status.temp = Math.round(data.celsius);
   });
+  var lcd = new arduino.LCD({
+    // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+    // Arduino pin # 12  11   5   4  3  2
+    pins: [ 12, 11, 5, 4, 3, 2 ],
+    rows: 2,
+    cols: 16
+  });
+  function printLCD() {
+    lcd.clear().cursor(0, 0);
+    lcd.print("Thermostat");
+    lcd.cursor(1, 0);
+    lcd.print("temperature: " + status.thermostat + "C");
+  }
+  printLCD();
 
   var app = express();
   var server = http.createServer(app);
@@ -30,28 +46,35 @@ board.on("ready", function() {
     status.light = 'ON';
     led.on();
     statusEmit();
-    // io.emit('ack button status', statusEmit);
   });
   app.get('/lightoff', function(req, res) {
     res.send('Light off');
     status.light = 'OFF';
     led.off();
     statusEmit();
-    // io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
   });
   app.get('/lock', function(req, res) {
     res.send('Door locked');
     status.door = 'locked';
     servo.to(1);
     statusEmit();
-    // io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
   });
   app.get('/unlock', function(req, res) {
     res.send('Door unlocked');
     status.door = 'unlocked';
     servo.max();
     statusEmit();
-    // io.emit('ack button status', { statusLight: status.light, statusDoor: status.door });
+  });
+  app.get('/thermostat/:thermTemp', function(req, res) {
+    var newThermTemp = req.params.thermTemp;
+    if (newThermTemp >= 0 && newThermTemp <= 30) {
+      res.send('The new thermostat temperature is ' + newThermTemp);
+      status.thermostat = newThermTemp;
+      printLCD();
+      statusEmit();
+    } else {
+      res.send(newThermTemp + ' is not a valid temperature');
+    }
   });
   app.get('/json', function(req, res) {
     res.json(status);
@@ -63,11 +86,11 @@ board.on("ready", function() {
   io.sockets.on('connection', function (socket) {
 
     setInterval(function(){
-      socket.emit('ack button status', { statusTemp: status.temp })
+      // socket.emit('ack button status', { statusTemp: status.temp, statusThermostat: status.thermostat })
+      statusEmit();
     }, 1000);
 
     //Set the current common status to the new client
-    statusEmit();
     // socket.emit('ack button status', { statusLight: status.light, statusDoor: status.door, statusTemp: status.temp });
 
     socket.on('button update event', function (data) {
@@ -91,10 +114,12 @@ board.on("ready", function() {
         status.door = 'unlocked';
         servo.max();
       }
+
+      if (data.statusThermostat != status.thermostat && data.statusThermostat != null) {
+        status.thermostat = data.statusThermostat;
+        printLCD();
+      }
       statusEmit();
-      // io.sockets.emit('ack button status', {
-      //   statusLight: status.light, statusDoor: status.door, statusTemp : status.temp
-      // });
     });
 
     //Info all clients if this client disconnect
